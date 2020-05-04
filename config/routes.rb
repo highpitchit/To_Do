1c0d41d44360e261b5b3a407a05e7c27627aa361
highpitchit <ameliafrancesnunn@gmail.com>
2020-05-05 00:06:49 +1000

    upload

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :todo_lists
+    resources :todo_items
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "todo_lists#index"
+
+end
